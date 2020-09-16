dabc0cd3dffeaf15d942cbd7430daffdc180d26c
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-09-16 19:28:23 +0300

    Updates

---
+++ precache-manifest.3bc175aac2444e9324ed3661e45c2600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a145626dc2d7d603c18d4ca891729498",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d56ab2e07d3660d7d132",
+    "url": "/todo/static/css/main.8200bb08.chunk.css"
+  },
+  {
+    "revision": "56e12b2b0ab6fce48f97",
+    "url": "/todo/static/js/2.9fb45872.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.9fb45872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56ab2e07d3660d7d132",
+    "url": "/todo/static/js/main.a907f599.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
